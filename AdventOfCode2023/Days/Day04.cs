b015e9a2af039f446122d2800a312fb9541323c9
Sebastian Rueth <sebastian.rueth@protonmail.com>
2023-12-04 20:34:19 +0100

    finish day 04

--- AdventOfCode2023/Days/Day04.cs
+++ AdventOfCode2023/Days/Day04.cs
@@ -14,7 +14,7 @@ public class Day04(string inputFileName) : AocChallenge(inputFileName)
         foreach (var card in cards)
         {
             var cardPoints = 0;
-            
+
             foreach (var number in card.NumbersWeHave)
             {
                 if (card.WinningNumbers.Contains(number))
@@ -22,20 +22,33 @@ public class Day04(string inputFileName) : AocChallenge(inputFileName)
                     cardPoints = cardPoints == 0 ? 1 : cardPoints * 2;
                 }
             }
-            
-            Console.WriteLine($"Card {card.Number} is worth {cardPoints}");
 
             cardPointSum += cardPoints;
         }
-        
+
         Console.WriteLine($"[DAY04][PT1] Total card points: {cardPointSum}");
     }
 
     public override void Part02()
     {
         var lines = Regex.Split(ChallengeFileString, "\r\n|\r|\n");
-        var cards = lines.Select(l => new Card(l));
-        
+        var cards = lines.Select(l => new Card(l)).ToArray();
+
+        for (var iCard = 0; iCard < cards.Length; iCard++)
+        {
+            var card = cards[iCard];
+            var winningNumbers = card.NumbersWeHave.Count(c => card.WinningNumbers.Contains(c));
+
+            // Increase the copy count of the next X cards by the amount of copies our current card has
+            for (var iCopyCard = iCard + 1; iCopyCard <= iCard + winningNumbers; iCopyCard++)
+            {
+                cards[iCopyCard].Copies += card.Copies;
+            }
+        }
+
+        var numScratchCards = cards.Aggregate(0, (cardCount, card) => cardCount + card.Copies);
+
+        Console.WriteLine($"[DAY04][PT2] Total number of cards: {numScratchCards}");
     }
 }
 
@@ -44,13 +57,15 @@ internal class Card
     public Card(string line)
     {
         var lineParts = line.Split(":");
-        
+
         Number = int.Parse(lineParts[0].Split(" ")[^1]);
 
         var cardNumbers = lineParts[1].Split("|");
         WinningNumbers = cardNumbers[0].Split(" ").Where(c => c != " " && c != "").Select(n => int.Parse(n.Trim())).ToArray();
         NumbersWeHave = cardNumbers[1].Split(" ").Where(c => c != " " && c != "").Select(n => int.Parse(n.Trim())).ToArray();
     }
+
+    public int Copies { get; set; } = 1;
 
     public int Number { get; set; }
     public int[] WinningNumbers { get; set; }
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -9,10 +9,7 @@ internal class Program
 
     static void Main(string[] args)
     {
-        var day04 = new Day04("Day04-example.txt");
-        day04.Part02();
-
-        //RunDaysSequential(UseExampleInput);
+        RunDaysSequential(UseExampleInput);
     }
 
     static void RunDaysSequential(bool useExampleInput)
